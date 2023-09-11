faaca3a30501bf0ebaf4a0ff7287d143d1aa2128
PaulaCR90 <p.a.castrorodriguez@gmail.com>
2023-09-11 13:06:39 +0200

    a

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <a href="https://www.facebook.com/profile.php?id=61550659575214&locale=es_ES">
           <img
             className="rrss"
-            src="../../../public/assets/logotipo-circular-de-facebook.png"
+            src="./public/assets/logotipo-circular-de-facebook.png"
             alt="facebook"
           />
           <p>Asociación Don Bigote</p>
@@ -21,7 +21,7 @@ const Footer = () => {
         <a href="https://www.instagram.com/donbigoteasoc/">
           <img
             className="rrss"
-            src="../../../public/assets/instagram.png"
+            src="./public/assets/instagram.png"
             alt="instagram"
           />
           <p>@donbigoteasoc</p>
@@ -29,7 +29,7 @@ const Footer = () => {
         <a href="mailto: asociaciondonbigote@gmail.com">
           <img
             className="rrss"
-            src="../../../public/assets/email.png"
+            src="./public/assets/email.png"
             alt="correo"
           />
           <p>asociaciondonbigote@gmail.com</p>
