d68f404b521b6b68ea083f5d77d9cc47933ea88b
PaulaCR90 <p.a.castrorodriguez@gmail.com>
2023-09-14 16:03:34 +0200

    add third section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Asociacion from "../components/Asociacion/Asociacion";
 import Razon from "../components/Razon/Razon";
+import Ser from "../components/Ser/ser";
 
 const Home = () => {
   return (
     <>
       <Asociacion />
       <Razon />
+      <Ser />
     </>
   );
 };
