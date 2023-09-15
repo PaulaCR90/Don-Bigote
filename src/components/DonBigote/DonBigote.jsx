a1791b9c57e671932b32fafddde265d7063f923e
PaulaCR90 <p.a.castrorodriguez@gmail.com>
2023-09-15 17:46:14 +0200

    Add responsive queries

--- src/components/DonBigote/DonBigote.jsx
+++ src/components/DonBigote/DonBigote.jsx
@@ -10,8 +10,7 @@ const DonBigote = () => {
           Porque nuestro espíritu manchego nos acompaña allá donde vamos y
           nuestro Don Bigote es el mejor ejemplo. Somos protección y defensa de
           los gatos, pero nuestro compromiso siempre va acompañado de un toque
-          de humor, compañerismo y solidaridad. Site sientes identificado/a, <a href="mailto: asociaciondonbigote@gmail.com">ÚNETE!</a> (algo con un toque de humor, cercano, amable, manchego)
-        </p>
+          de humor, compañerismo y solidaridad. Si te sientes identificado/a, <a href="mailto: asociaciondonbigote@gmail.com">ÚNETE!</a></p>
       </div>
       <img src="" alt="" />
     </section>
